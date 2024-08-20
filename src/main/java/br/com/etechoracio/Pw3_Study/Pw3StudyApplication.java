c0d3890014e9db601f6b0d7be35b6f3546cc8696
Anderson-Armando <arevalosanderson9@gmail.com>
2024-08-20 11:01:53 -0300

    Criação dos componentes

---
+++ src/main/java/br/com/etechoracio/Pw3_Study/Pw3StudyApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.Pw3_Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw3StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etechoracio/Pw3_Study/Pw3StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.Pw3_Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
